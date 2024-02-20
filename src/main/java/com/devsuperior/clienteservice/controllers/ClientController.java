33dba4eb720e24a4528356fa0956f5714e0fb5b8
Gabriel <l44.gabriel@hotmail.com>
2024-02-20 09:58:13 -0300

    PUT client

--- src/main/java/com/devsuperior/clienteservice/controllers/ClientController.java
+++ src/main/java/com/devsuperior/clienteservice/controllers/ClientController.java
@@ -32,8 +32,13 @@ public class ClientController {
     @PostMapping()
     public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
         dto =  service.insert(dto);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(dto.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto =  service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
